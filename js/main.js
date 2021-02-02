33d1aee6318b2573df8577c29be5412d2c77f71c
Florian Leroux <florian.leroux3@laposte.net>
2021-02-02 16:59:02 +0100

    perf(pwa): improved lighthouse score

--- js/main.js
+++ js/main.js
@@ -151,7 +151,7 @@ $(document).ready(function () {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("serviceWorker.js")
+      .register("/serviceWorker.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log(err));
   });
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,15 +1,16 @@
 const staticApp = "floriaaan-app";
 const assets = [
-  "index.html",
-  "css/style.css",
-  "css/responsive.css",
-  "js/main.js",
-  "js/uses.js",
-  "lib/jquery/jquery.js",
-  "lib/jquery/jquery-migrate.js",
-  "lib/typed/typed.js",
-  "images/home-bg.png",
-  "images/logo.png",
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/css/responsive.css",
+  "/js/main.js",
+  "/js/uses.js",
+  "/lib/jquery/jquery.min.js",
+  "/lib/jquery/jquery-migrate.min.js",
+  "/lib/typed/typed.js",
+  "/images/home-bg.png",
+  "/images/logo.png",
 ];
 
 self.addEventListener("install", (installEvent) => {
